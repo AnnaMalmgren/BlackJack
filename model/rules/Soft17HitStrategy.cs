b146162df137cc8297d7be13ccb7094e2a764aa4
Anna Malmgren <am223rj@student.lnu.se>
2019-10-23 00:14:54 +0200

    Adding class soft17strategy

---
+++ model/rules/Soft17HitStrategy.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17HitStrategy : IHitStrategy
+    {
+        public bool DoHit(model.Player a_dealer) {
+            throw new NotImplementedException();
+        }
+    }
+}
