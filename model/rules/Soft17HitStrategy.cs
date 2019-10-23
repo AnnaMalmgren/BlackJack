eac9915f8e072b2ac5cc08bbc6235ce058e8b3fb
Anna Malmgren <am223rj@student.lnu.se>
2019-10-23 22:34:59 +0200

    Adding Soft17Strategy

--- model/rules/Soft17HitStrategy.cs
+++ model/rules/Soft17HitStrategy.cs
@@ -7,8 +7,22 @@ namespace BlackJack.model.rules
 {
     class Soft17HitStrategy : IHitStrategy
     {
-        public bool DoHit(model.Player a_dealer) {
-            throw new NotImplementedException();
+        private const int g_hitLimit = 17;
+        public bool DoHit(model.Player a_dealer) 
+        {
+            List<Card> hand = a_dealer.GetHand().ToList();
+            
+            if (a_dealer.CalcScore() == g_hitLimit)
+            {
+                return this.DoesHandContainAce(hand);
+            }
+
+            return a_dealer.CalcScore() < g_hitLimit;
+        }
+
+        private bool DoesHandContainAce(List<Card> hand)
+        {
+            return hand.Exists(card => card.GetValue() == Card.Value.Ace);
         }
     }
 }
